691de1bb5aa2ed723d2f8802d8a8bb1cea1291ad
donghunee <ehdgns1766@naver.com>
2019-10-06 23:02:59 +0900

    money

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+      <div className="main">
+        <div className="header">
+          <div className="headerText">Sopt_Money</div>
+          </div>
+          <div className="body">
+            <div className="content">
+              <div></div>
+            </div>
+            <div className="content">요시요시</div>
+            <div className="content">요시요시</div>
+        </div>
+    </div>
+    );
+};
+
+export default Home;
